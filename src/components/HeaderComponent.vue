59b849cfe196685cd744335d279e16914c08e6dd
Aayush Niraula <aayush@accessworld.net>
2022-07-28 15:03:33 +0545

    header & logout, session clear

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>Add Restaurant</li>
+            <li>Update Restaurant</li>
+            <li><a v-on:click="logout"> Logout</a></li>
+        </ul>
+    </nav>
+</template>
+<script>
+export default {
+    name:'HeaderComponent',
+    methods:{
+        logout(){
+            localStorage.clear();
+            this.$router.push({name:'SignIn'});
+        }
+    }
+}
+</script>
+<style>
+    nav{
+        background: #8b0304;
+        padding: 10px;
+        margin-top: -10px !important;
+        margin-left: -8px !important;
+        margin-right: -8px!important;
+        max-width: 100% !important;
+    }
+    nav ul{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        align-items: center;
+        list-style-type: none;
+    }
+    nav ul li, nav ul li a{
+        font-size: 20px;
+        padding: 10px 15px;
+        text-transform: uppercase;
+        color: #fff;
+        cursor: pointer;
+    }
+    nav ul li:hover, nav ul li:hover a{
+        background-color: #fff;
+        color: #8b0304;
+    }
+    nav ul li a{
+        text-decoration: none;
+    }
+</style>
--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,16 +1,19 @@
 <template>
+    <HeaderComponent/>
     <h1>Hi, Welcome to Home Page</h1>
 </template>
 <script>
+import HeaderComponent from './HeaderComponent.vue';
 export default {
-    name:'HomeComponent',
-    mounted(){
+    name: "HomeComponent",
+    mounted() {
         // if not logged in do not show redirect to homepage
         // Get user from local storage
-        let user = localStorage.getItem('user-info');
-        if(!user){
-            this.$router.push({name:'SignIn'});
+        let user = localStorage.getItem("user-info");
+        if (!user) {
+            this.$router.push({ name: "SignIn" });
         }
-    }
+    },
+    components: { HeaderComponent }
 }
 </script>
