4dc171bef2240067a274a3090b4b2a96d5e72e03
Aayush Niraula <aayush@accessworld.net>
2022-07-29 10:13:01 +0545

    Prefill form

--- src/components/UpdateComponent.vue
+++ src/components/UpdateComponent.vue
@@ -1,19 +1,40 @@
 <template>
     <HeaderComponent/>
     <h1>Update Restaurant</h1>
+    <div class="error">
+    </div>
+    <form class="add-restro-form">
+        <input type="text" name="name" placeholder="Enter restaurant's name" v-model="restaurant.name">
+        <input type="text" name="address" placeholder="Enter restaurant's address" v-model="restaurant.address">
+        <input type="tel" name="contact" placeholder="Enter restaurant's contact" v-model="restaurant.contact">
+        <button>Update</button>
+    </form>
 </template>
 <script>
 import HeaderComponent from './HeaderComponent.vue';
+import axios from 'axios';
 export default {
     name: "UpdateComponent",
     components: { HeaderComponent },
-    mounted() {
+    data(){
+        return{
+            restaurant:[],
+            errors:[]
+        }
+    },
+    async mounted() {
         // if not logged in do not show redirect to homepage
         // Get user from local storage
         let user = localStorage.getItem("user-info");
         if (!user) {
             this.$router.push({ name: "SignIn" });
         }
+        // Prefill form
+        // console.log(this.$route.params.id)
+        let restro_id = this.$route.params.id
+        let result = await axios.get('http://localhost:3000/restaurants/'+restro_id);
+        // console.log(result);
+        this.restaurant = result.data;
     }
 }
 </script>
